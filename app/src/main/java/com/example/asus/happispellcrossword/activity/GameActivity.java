edfcfde69d554dce4d9962cfea21951b03b3b61f
nguyenson2012 <traibachkhoa2012@gmail.com>
2016-06-10 14:01:12 +0700

    add Gridview and image for question

--- app/src/main/java/com/example/asus/happispellcrossword/activity/GameActivity.java
+++ app/src/main/java/com/example/asus/happispellcrossword/activity/GameActivity.java
@@ -1,17 +1,210 @@
 package com.example.asus.happispellcrossword.activity;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.asus.happispellcrossword.R;
+import com.example.asus.happispellcrossword.adapter.GridviewAdapter;
+import com.example.asus.happispellcrossword.model.StaticVariable;
+import com.example.asus.happispellcrossword.model.WordObject;
+import com.example.asus.happispellcrossword.model.WordObjectsManager;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+
+import java.util.ArrayList;
 
 /**
  * Created by Asus on 6/8/2016.
  */
-public class GameActivity extends AppCompatActivity{
+public class GameActivity extends Activity implements GridviewAdapter.OnItemGridViewClick {
+    public static final int AD_HEIGHT = 50;
+    public static final int NUM_OF_COLLUMN = 10;
+    public static final int NUM_OF_ROW = NUM_OF_COLLUMN;
+    public static final int MAX_NUM_OF_KEYBOARD_BTN_PER_ROW = 10;
+    private static GridView gridView;
+    //    public int PARENT_VERTICAL_MARGIN;
+    private int LINE_HEIGHT;
+    private int BTN_KEYBOARD_MARGIN_LEFT_AND_RIGHT;
+    private int BTN_KEYBOARD_EDGE_SIZE;
+    private int screenWidth = 0;
+    private int screenHeight = 0;
+    private TextView txtView_question;
+    private ImageView imgView_question;
+    private ImageButton del_btn;
+    private String[][] gridViewData = new String[NUM_OF_COLLUMN][NUM_OF_ROW];//gridViewData[x][y]
+    private WordObjectsManager objManger = WordObjectsManager.getInstance();
+    private GridviewAdapter adapter;
+    private ImageButton btCheckAnswer;
+    //    private Button btSolve;
+    private ImageButton btClear;
+    private DisplayImageOptions opt;
+    private ImageLoader imageLoader;
+    private AdView mAdView;
+    //    private ArrayList<WordObject> listQuestion;
+    private ArrayList<Bitmap> listBitmapImageQuestion;
+    private StaticVariable staticVariable;
+    private String prefName = "data";
+    private int doneLevel;
+    private int currentLevel=1;
+    private int timeStartLevel = 0;
+    private int timeCompleteLevel = 0;
+    private boolean allLevelDone = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        staticVariable = StaticVariable.getInstance();
+        timeStartLevel = (int) (System.currentTimeMillis() / 1000);
+//        context = this;
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        screenWidth = metrics.widthPixels;
+        screenHeight = metrics.heightPixels;
+        setupGridView();
+        LINE_HEIGHT = (screenHeight - gridView.getMinimumHeight())/5;
+        BTN_KEYBOARD_MARGIN_LEFT_AND_RIGHT = LINE_HEIGHT/8; // If need to adjust, change here
+        BTN_KEYBOARD_EDGE_SIZE = LINE_HEIGHT-2*BTN_KEYBOARD_MARGIN_LEFT_AND_RIGHT;
+        //If the size above is not correct
+        if(BTN_KEYBOARD_EDGE_SIZE*MAX_NUM_OF_KEYBOARD_BTN_PER_ROW+
+                BTN_KEYBOARD_MARGIN_LEFT_AND_RIGHT*(MAX_NUM_OF_KEYBOARD_BTN_PER_ROW+1)>screenWidth)
+        {
+            int temp = screenWidth/((MAX_NUM_OF_KEYBOARD_BTN_PER_ROW));
+            BTN_KEYBOARD_MARGIN_LEFT_AND_RIGHT = temp/8; // If need to adjust, change here
+            BTN_KEYBOARD_EDGE_SIZE = temp-2*BTN_KEYBOARD_MARGIN_LEFT_AND_RIGHT;
+        }
     }
+
+    private void initializeQuestion() {
+        objManger.setObjectArrayList(new ArrayList<WordObject>());
+        objManger.setObjectArrayList(staticVariable.getAllStage().get(currentLevel - 1).getListQuestion());
+    }
+
+    private void setupGridView() {
+        initializeQuestion();
+        //Reset gridview
+        for (int i = 0; i < gridViewData.length; i++) {
+            for (int j = 0; j < gridViewData[0].length; j++)//the board is rectangular
+            {
+                WordObject temp = objManger.getObjectAt(i, j);
+                if (temp != null) {
+                    gridViewData[i][j] = GridviewAdapter.ENABLE;
+//                    Log.e("OBJ","i,j = "+i+" , "+j);
+                } else {
+                    gridViewData[i][j] = GridviewAdapter.DISABLE;
+//                    Log.e("NULL","i,j = "+i+" , "+j);
+                }
+            }
+        }
+
+        adapter = new GridviewAdapter(this, gridViewData);
+        adapter.setUpListWord(objManger.getObjectArrayList());
+        gridView = (GridView) findViewById(R.id.gridview_puzzle);
+        gridView.setMinimumHeight(screenWidth);
+        gridView.setAdapter(adapter);
+        gridView.setNumColumns(NUM_OF_COLLUMN);
+//        gridView.setColumnWidth((int) ((gridView.getWidth() / NUM_OF_COLLUMN) * 0.9));
+        gridView.setMinimumWidth(screenWidth);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //    @Override
+    public void onItemGridViewClick(int position) {
+        int positionX = position % NUM_OF_COLLUMN;
+        int positionY = position / NUM_OF_COLLUMN;}
+
+    private boolean checkAnswer() {
+        boolean checkAnswer = true;
+        for (WordObject question : objManger.getObjectArrayList()) {
+            String answer = "";
+            int firstX = question.startX;
+            int firstY = question.startY;
+            if (question.getOrientation() == WordObject.HORIZONTAL) {
+                for (int i = 0; i < question.getResult().length(); i++) {
+                    answer = answer.concat(gridViewData[firstX + i][firstY]);
+                }
+                if (!answer.equals(question.getResult()))
+                    checkAnswer = false;
+            } else {
+                for (int i = 0; i < question.getResult().length(); i++) {
+                    answer = answer.concat(gridViewData[firstX][firstY + i]);
+                }
+                if (!answer.equals(question.getResult()))
+                    checkAnswer = false;
+            }
+
+        }
+        return checkAnswer;
+    }
+
+    private void clearLevel() {
+        for (WordObject question : objManger.getObjectArrayList()) {
+            int firstX = question.startX;
+            int firstY = question.startY;
+            if (question.getOrientation() == WordObject.HORIZONTAL) {
+                for (int i = 0; i < question.getResult().length(); i++) {
+                    gridViewData[firstX + i][firstY] = "";
+                }
+            } else {
+                for (int i = 0; i < question.getResult().length(); i++) {
+                    gridViewData[firstX][firstY + i] = "";
+                }
+            }
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/asus/happispellcrossword/adapter/GridviewAdapter.java
@@ -0,0 +1,264 @@
+package com.example.asus.happispellcrossword.adapter;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.asus.happispellcrossword.R;
+import com.example.asus.happispellcrossword.activity.GameActivity;
+import com.example.asus.happispellcrossword.model.WordObject;
+import com.example.asus.happispellcrossword.model.WordObjectsManager;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ThangDuong on 12-May-16.
+ */
+public class GridviewAdapter extends BaseAdapter {
+    public static final String DISABLE = "0";
+    public static final String ENABLE = "";
+    public static final int NORMAL = 2;
+    public static final int MAIN_CLICKED = 3;
+    public static final int SUB_CLICKED = 4;
+    public static final int TRANSPARENT = 5;
+    private static int color[][];//The board must be rectangular + data[x][y]
+    private static boolean isAnimation[][];//The board must be rectangular + data[x][y]
+    private static WordObject clickedOject = null;
+    ObjectAnimator scaleAnimationX = new ObjectAnimator();
+    ObjectAnimator scaleAnimationY = new ObjectAnimator();
+    //Constructor to resetColor values
+    WordObjectsManager objManager = WordObjectsManager.getInstance();
+    private Context context;
+    private String data[][];//The board must be rectangular + data[x][y]
+    private ArrayList<WordObject> listWord = new ArrayList<WordObject>();
+    private OnItemGridViewClick gridViewClickListener;
+    private Animation animation;
+    private DisplayImageOptions opt;
+
+    public GridviewAdapter(Activity context, String[][] data) {
+
+        this.gridViewClickListener = (OnItemGridViewClick) context;
+        this.context = context;
+        this.data = data;
+        color = new int[data.length][data[0].length];
+        isAnimation = new boolean[data.length][data[0].length];
+        for (int i = 0; i < color.length; i++) {
+            for (int j = 0; j < color[0].length; j++)//the board is rectangular
+            {
+                isAnimation[i][j] = false;
+            }
+        }
+        resetColor();
+        onClickCell(objManager.get(0).startX, objManager.get(0).startY);
+    }
+
+    public void resetColor() {
+        for (int i = 0; i < color.length; i++) {
+            for (int j = 0; j < color[0].length; j++)//the board is rectangular
+            {
+                WordObject obj = objManager.getObjectAt(i, j);
+                if (obj != null)
+                    color[i][j] = NORMAL;
+                else
+                    color[i][j] = TRANSPARENT;
+            }
+        }
+        notifyDataSetChanged();
+    }
+
+
+    @Override
+    public int getCount() {
+//        Log.e("GridviewAdapter","data.length = "+data.length+" , data[0].length = "+data[0].length);
+        return data.length * data[0].length;//The board must be rectangular
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        // LayoutInflater to call external grid_item.xml file
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View gridView;
+
+        if (convertView == null) {
+            gridView = new View(context);
+            // get layout from grid_item.xml ( Defined Below )
+            gridView = inflater.inflate(R.layout.grid_puzzle_item, null);
+        } else {
+            gridView = convertView;
+
+        }
+
+        final Button cell = (Button) gridView.findViewById(R.id.button);
+        final TextView textViewNumberQuestion = (TextView) gridView.findViewById(R.id.tvItemSTT);
+
+        //set Row Height
+        DisplayMetrics metrics = new DisplayMetrics();
+        metrics = context.getResources().getDisplayMetrics();
+        cell.setMinimumHeight(0);
+//        cell.setHeight(MainActivity.getRowHeight());
+        cell.setHeight((int) ((metrics.widthPixels / GameActivity.NUM_OF_ROW) * 0.9));
+
+        //set default text
+//        cell.setText(Integer.toString(position));
+        cell.setTextColor(Color.BLACK);
+        final int positionX = position % GameActivity.NUM_OF_COLLUMN;
+        final int positionY = position / GameActivity.NUM_OF_COLLUMN;
+        if(data[positionX][positionY]==GridviewAdapter.DISABLE){
+            cell.setVisibility(View.INVISIBLE);
+        }
+        for(WordObject wordObject:listWord){
+            int positionImage=0;
+            if(wordObject.getOrientation()==WordObject.HORIZONTAL){
+                positionImage=wordObject.startX+wordObject.startY*10+wordObject.getResult().length();
+            }else
+                positionImage=wordObject.startX+(wordObject.startY+wordObject.getResult().length())*10;
+            if(position==positionImage){
+                cell.setVisibility(View.VISIBLE);
+                cell.setBackgroundResource(R.drawable.ic_action_word);
+            }
+        }
+        return gridView;
+    }
+
+    private void onClickCell(int x, int y) {
+        //Reset color of the grid
+        resetColor();
+
+        WordObject obj = objManager.getObjectAt(x, y);
+        if (obj != null)// If clicked inside a word
+        {
+            obj.setClickedPosition(x, y);
+            clickedOject = obj;
+            colorSurroundCells(x, y);
+
+        }
+    }
+
+    private void colorSurroundCells(int x, int y)// x,y is the starting point, cell is the work obj to check its length
+    {
+        color[x][y] = MAIN_CLICKED;
+        int tempX = x + 1;// color the subclicked on the right
+        while (tempX < data.length && clickedOject.isInsideWord(tempX, y)) {
+            color[tempX][y] = SUB_CLICKED;
+            tempX++;
+        }
+        tempX = x - 1;// color the subclicked on the right
+        while (tempX >= 0 && clickedOject.isInsideWord(tempX, y)) {
+            color[tempX][y] = SUB_CLICKED;
+            tempX--;
+        }
+
+        int tempY = y + 1;// color the subclicked downward
+        while (tempY < data[0].length && clickedOject.isInsideWord(x, tempY)) {
+            color[x][tempY] = SUB_CLICKED;
+            tempY++;
+        }
+        tempY = y - 1;// color the subclicked upward
+        while (tempY >= 0 && clickedOject.isInsideWord(x, tempY)) {
+            color[x][tempY] = SUB_CLICKED;
+            tempY--;
+        }
+        notifyDataSetChanged();
+    }
+
+    public void nextClickedPosition() {
+        int lastClickedX = WordObject.getClickedPositionX();
+        int lastClickedY = WordObject.getClickedPositionY();
+        //Horizontal and stop at last digit
+        if (clickedOject.getOrientation() == WordObject.HORIZONTAL
+                && WordObject.getClickedPositionX() <= clickedOject.startX + clickedOject.getResult().length() - 2) {
+            if (lastClickedX < clickedOject.startX + clickedOject.getResult().length())
+                lastClickedX++;
+        }
+
+        //Vertical and stop at last digit
+        if (clickedOject.getOrientation() == WordObject.VERTICAL
+                && WordObject.getClickedPositionY() <= clickedOject.startY + clickedOject.getResult().length() - 2) {
+            if (lastClickedY < clickedOject.startY + clickedOject.getResult().length())
+                lastClickedY++;
+        }
+        clickedOject.setClickedPosition(lastClickedX, lastClickedY);
+
+//        onClickCell(WordObject.getClickedPositionX(), WordObject.getClickedPositionY());
+
+        colorSurroundCells(lastClickedX, lastClickedY);
+    }
+
+    public void backClickedPosition() {
+        int lastClickedX = WordObject.getClickedPositionX();
+        int lastClickedY = WordObject.getClickedPositionY();
+        //Horizontal and stop at last digit
+        if (clickedOject.getOrientation() == WordObject.HORIZONTAL
+                && WordObject.getClickedPositionX() < clickedOject.startX + clickedOject.getResult().length()) {
+            if (lastClickedX > clickedOject.startX)
+                lastClickedX--;
+        }
+
+        //Vertical and stop at last digit
+        if (clickedOject.getOrientation() == WordObject.VERTICAL
+                && WordObject.getClickedPositionY() < clickedOject.startY + clickedOject.getResult().length()) {
+            if (lastClickedY > clickedOject.startY)
+                lastClickedY--;
+        }
+        clickedOject.setClickedPosition(lastClickedX, lastClickedY);
+
+//        onClickCell(WordObject.getClickedPositionX(), WordObject.getClickedPositionY());
+
+        colorSurroundCells(lastClickedX, lastClickedY);
+    }
+
+    private void settingAnimation() {
+        scaleAnimationX.setStartDelay(0);
+        scaleAnimationX.setRepeatCount(0);
+        scaleAnimationX.setRepeatMode(ValueAnimator.REVERSE);
+        scaleAnimationX.setInterpolator(new LinearInterpolator());
+        scaleAnimationY.setStartDelay(0);
+        scaleAnimationY.setRepeatCount(0);
+        scaleAnimationY.setRepeatMode(ValueAnimator.REVERSE);
+        scaleAnimationY.setInterpolator(new LinearInterpolator());
+    }
+
+    private void startScaleAnimation(View view, int duration) {
+        scaleAnimationX = ObjectAnimator.ofFloat(view, "scaleX", new float[]{0.5f, 1.0f}).setDuration(duration);
+        scaleAnimationY = ObjectAnimator.ofFloat(view, "scaleY", new float[]{0.5f, 1.0f}).setDuration(duration);
+        final AnimatorSet animation = new AnimatorSet();
+        animation.playTogether(scaleAnimationX, scaleAnimationY);
+        animation.start();
+    }
+
+    public void setUpListWord(ArrayList<WordObject> listWord) {
+        this.listWord = listWord;
+    }
+
+    public interface OnItemGridViewClick {
+        void onItemGridViewClick(int position);
+    }
+}
